59c49c21083d86bbb843dd583f205ba9aeb1d7f8
alex-zaitcev11 <aleksandrzajcev697@gmail.com>
2022-07-26 20:40:32 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Locale;
+public class Main {
+    public static void main(String [] args) {
+        String a = "Phyton Exercise";
+        String b = "Phyton Exercises";
+        boolean a1 = a.endsWith("se");
+        boolean b1 = b.endsWith("se");
+        System.out.println(a + " " + a1);
+        System.out.println(b + " " + b1);
+    }
+}
